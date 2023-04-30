9e0375509d4d25584490bfa95c7a92263c5befef
imstylen <michael.steijlen@gmail.com>
2023-04-29 21:43:04 -0400

    add drawing to main

--- Main.py
+++ Main.py
@@ -4,6 +4,12 @@ from PipeFilter import Pipe
 from keychain import API_KEY
 
 
+#   filter           pipe        filter
+# (generator)===>===>===>===>===(ranker)
+# 
+# generation prompt -> out1.txt -> ranker prompt -> out2.txt
+
+
 
 idea_generator = PartyIdeaGenerator(API_KEY,"out/out1.txt",3)
 idea_ranker = PartyIdeaRanker(API_KEY,"out/out2.txt")
